138273c54df06d9e5658ff803a2c6cece70647b5
Carolina Romero Seguame <132811545+CarolinaRomeroSeguame12@users.noreply.github.com>
2023-07-29 03:09:56 -0700

    Add files via upload

---
+++ acticidad2.cpp
@@ -0,0 +1,34 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#include <stdio.h>
+#include <iostream>
+int main(){ 
+    
+    float numero1, numero2;
+
+std::cout 
+<< "Escribe el primer numero: ";
+ std::cin >> numero1;
+std::cout << "Escribe el segundo numero: ";
+ std:: cin>> numero2;
+
+float suma = numero1 + numero2;
+float resta = numero1 - numero2;
+float multiplicacion = numero1 * numero2;
+float division = numero1 / numero2;
+
+std::cout << "La suma es:" << suma << std::endl;
+std::cout << "La resta es:"<< resta << std::endl;
+std::cout << "La multiplicación es:" << multiplicacion <<std::endl;
+std::cout << "La división es:" << division << std::endl;
+
+    return 0;
+}
+
